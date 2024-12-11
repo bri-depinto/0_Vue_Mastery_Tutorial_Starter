5d12da60650b6b3fe002d5152f51ab8276c34271
bri.depinto <briannadepinto@gmail.com>
2024-12-11 12:42:57 -0500

    List Rendering

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
+            cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 8
+            image: './assets/images/socks_blue.jpg',
+            inventory: 10,
+            inStock: 10,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ]
         }
     }
 })
